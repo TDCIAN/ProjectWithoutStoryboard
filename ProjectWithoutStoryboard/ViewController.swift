cd6712acd8e917e360665cced7775b2fe3e55e0d
JMKim <tdcian71@gmail.com>
2024-07-27 19:45:16 +0900

    made sample project

---
+++ ProjectWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectWithoutStoryboard
+//
+//  Created by 김정민 on 7/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemYellow
+    }
+
+
+}
+
